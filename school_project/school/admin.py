d7fe6b9829488f4ae60689a3112885e319731b23
Lukaandriadze <147325836+Lukaandriadze@users.noreply.github.com>
2024-11-16 09:40:25 -0800

    commit

---
+++ school_project/school/admin.py
@@ -0,0 +1,7 @@
+# school/admin.py
+from django.contrib import admin
+from .models import Student, Teacher, Class
+
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Class)
